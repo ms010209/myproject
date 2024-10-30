f8232dc800cfc2f410b655cade84916676ac891c
ms010209 <129668058+ms010209@users.noreply.github.com>
2024-10-30 16:17:05 +0900

    24/10/30 16:16 기초 틀

---
+++ src/main/java/com/project/myproject/Controller/MainController.java
@@ -0,0 +1,12 @@
+package com.project.myproject.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+}
